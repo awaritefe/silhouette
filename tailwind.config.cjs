1e696ffc926bc8d911c48e866c9d1403d6f87069
dawaritefe <daniel.awaritefe@biglight.co.uk>
2024-06-06 18:10:58 +0100

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./slices/**/*.{html,js,ts,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Nunito Sans Variable", "sans-serif"],
+        header: ["Nunito Variable", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
