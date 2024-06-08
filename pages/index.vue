baf6a0b86d2c1ba982988047c71355e81fd9b15b
dawaritefe <daniel.awaritefe@biglight.co.uk>
2024-06-08 18:45:58 +0100

    Feat: Homepage added

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { components } from '~/slices'
+import { components } from "~/slices";
 
-const prismic = usePrismic()
-const { data: page } = useAsyncData('index', () =>
-  prismic.client.getByUID('page', 'home')
-)
+const prismic = usePrismic();
+const { data: page } = useAsyncData("index", () =>
+  prismic.client.getByUID("page", "home")
+);
 
-useHead({
-  title: prismic.asText(page.value?.data.title)
-})
+async function init() {
+  await useSeoMeta({
+    title: page.value?.data.meta_title ?? undefined,
+    description: page.value?.data.meta_description ?? undefined,
+    ogImage: prismic.asImageSrc(page.value?.data.meta_image ?? undefined),
+  });
+}
+init();
 </script>
-
 
 <template>
   <SliceZone
