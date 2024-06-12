2c265581a9d8b20132081ba6fce04ac32c70c3a9
dawaritefe <daniel.awaritefe@biglight.co.uk>
2024-06-12 16:23:59 +0100

    Feat: setup uid page

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
-import { components } from '~/slices'
+import { components } from "~/slices";
 
-const prismic = usePrismic()
-const route = useRoute()
+const prismic = usePrismic();
+const route = useRoute();
 const { data: page } = useAsyncData(route.params.uid as string, () =>
-  prismic.client.getByUID('page', route.params.uid as string)
-)
+  prismic.client.getByUID("page", route.params.uid as string, {
+    fetchLinks: [
+      "testimonial.quote",
+      "testimonial.avatar",
+      "testimonial.name",
+      "testimonial.job_title",
+    ],
+  })
+);
 
-useHead({
-  title: prismic.asText(page.value?.data.title)
-})
+async function init() {
+  await useSeoMeta({
+    title: page.value?.data.meta_title ?? undefined,
+    description: page.value?.data.meta_description ?? undefined,
+    ogImage: prismic.asImageSrc(page.value?.data.meta_image ?? undefined),
+  });
+}
+init();
 </script>
-
 
 <template>
   <SliceZone
